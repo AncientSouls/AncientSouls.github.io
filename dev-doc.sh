cd79793f7e0efd0c6362b52047f83ef49b055bb8
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-04 00:02:16 +0000

    travis

--- dev-doc.sh
+++ dev-doc.sh
@@ -1,6 +1,6 @@
 find ./doc/ ! -name '.git' ! -name 'doc' -exec rm -rf {} +
 
-typedoc --name "AncientSouls Documentation" --module commonjs --out ./doc/ --mode file --excludeExternals --externalPattern "**/node_modules/**" --exclude "**/node_modules/**" --disableOutputCheck --ignoreCompilerErrors ./packages
+typedoc --name "AncientSouls Documentation" --module commonjs --out ./doc/ --excludeExternals --exclude "**/+(node_modules|tests)/**" --disableOutputCheck --ignoreCompilerErrors ./packages
 
 for p in ./packages/*
 do
--- dev-normalize.sh
+++ dev-normalize.sh
@@ -2,4 +2,5 @@ for p in ./packages/*
 do
 cp ./.gitignore $p/.gitignore
 cp ./.npmignore $p/.npmignore
+cp ./.travis.yml $p/.travis.yml
 done
